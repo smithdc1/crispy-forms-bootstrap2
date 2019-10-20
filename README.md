89a494e96267e8123fc8fb26a7ee3ee89d2d2687
David Smith <smithdc@gmail.com>
2019-10-20 09:26:15 +0100

    first submit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Crispy-forms-boostrap2
+
+Django-crispy-forms bootstrap2 templates
+
+TODO
+# Install Instructions
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crispy-forms-bootstrap2',
+    version='v.01',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='',
+    author_email='',
+    description='Django-crispy-forms bootstrap2 templates',
+    install_requires=['django-crispy-forms >= 1.8'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
